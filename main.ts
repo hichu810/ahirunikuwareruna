262296b1f434af18d5ff8fa3186c9b145d226578
hichu810 <83585306+hichu810@users.noreply.github.com>
2021-05-19 15:24:25 +0900

    更新 main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairWest, function (sprite, location) {
     tiles.setTilemap(tilemap`レベル1`)
-    game.splash("何なのあれ、まじで、とりま逃げよう")
+    game.showLongText("何なのあれ", DialogLayout.Bottom)
+    game.showLongText("アヒルが追いかけてきた", DialogLayout.Bottom)
+    game.showLongText("食われるなってこうゆうことか", DialogLayout.Bottom)
+    game.showLongText("早く出よう", DialogLayout.Bottom)
+    tiles.placeOnRandomTile(mySprite2, sprites.dungeon.collectibleInsignia)
     info.stopCountdown()
     info.startCountdown(10)
     tiles.placeOnRandomTile(mySprite, sprites.castle.tileGrass3)
@@ -8,18 +12,79 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairWest, function (spri
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleBlueCrystal, function (sprite, location) {
     tiles.setTilemap(tilemap`レベル5`)
+    tiles.placeOnRandomTile(mySprite, sprites.dungeon.hazardLava0)
+    game.setDialogCursor(img`
+        . . . . . . . . . b 5 b . . . . 
+        . . . . . . . . . b 5 b . . . . 
+        . . . . . . b b b b b b . . . . 
+        . . . . . b b 5 5 5 5 5 b . . . 
+        . . . . b b 5 b c 5 5 d 4 c . . 
+        . b b b b 5 5 5 b f d d 4 4 4 b 
+        . b d 5 b 5 5 b c b 4 4 4 4 b . 
+        . . b 5 5 b 5 5 5 4 4 4 4 b . . 
+        . . b d 5 5 b 5 5 5 5 5 5 b . . 
+        . b d b 5 5 5 d 5 5 5 5 5 5 b . 
+        b d d c d 5 5 b 5 5 5 5 5 5 b . 
+        c d d d c c b 5 5 5 5 5 5 5 b . 
+        c b d d d d d 5 5 5 5 5 5 5 b . 
+        . c d d d d d d 5 5 5 5 5 d b . 
+        . . c b d d d d d 5 5 5 b b . . 
+        . . . c c c c c c c c b b . . . 
+        `)
+    game.showLongText("ここがさいごだ", DialogLayout.Bottom)
+    game.setDialogCursor(img`
+        . . . . f f f f . . . . 
+        . . f f e e e e f f . . 
+        . f f e e e e e e f f . 
+        f f f f 4 e e e f f f f 
+        f f f 4 4 4 e e f f f f 
+        f f f 4 4 4 4 e e f f f 
+        f 4 e 4 4 4 4 4 4 e 4 f 
+        f 4 4 f f 4 4 f f 4 4 f 
+        f e 4 d d d d d d 4 e f 
+        . f e d d b b d d e f . 
+        . f f e 4 4 4 4 e f f . 
+        e 4 f b 1 1 1 1 b f 4 e 
+        4 d f 1 1 1 1 1 1 f d 4 
+        4 4 f 6 6 6 6 6 6 f 4 4 
+        . . . f f f f f f . . . 
+        . . . f f . . f f . . . 
+        `)
+    game.showLongText("え、まじ", DialogLayout.Bottom)
+    game.setDialogCursor(img`
+        . . . . . . . . . b 5 b . . . . 
+        . . . . . . . . . b 5 b . . . . 
+        . . . . . . b b b b b b . . . . 
+        . . . . . b b 5 5 5 5 5 b . . . 
+        . . . . b b 5 b c 5 5 d 4 c . . 
+        . b b b b 5 5 5 b f d d 4 4 4 b 
+        . b d 5 b 5 5 b c b 4 4 4 4 b . 
+        . . b 5 5 b 5 5 5 4 4 4 4 b . . 
+        . . b d 5 5 b 5 5 5 5 5 5 b . . 
+        . b d b 5 5 5 d 5 5 5 5 5 5 b . 
+        b d d c d 5 5 b 5 5 5 5 5 5 b . 
+        c d d d c c b 5 5 5 5 5 5 5 b . 
+        c b d d d d d 5 5 5 5 5 5 5 b . 
+        . c d d d d d d 5 5 5 5 5 d b . 
+        . . c b d d d d d 5 5 5 b b . . 
+        . . . c c c c c c c c b b . . . 
+        `)
+    game.showLongText("ああ、だがわたしも本気で", DialogLayout.Bottom)
+    game.showLongText("いかせてもらう", DialogLayout.Bottom)
+    tiles.placeOnRandomTile(mySprite2, sprites.dungeon.collectibleInsignia)
     info.stopCountdown()
     info.startCountdown(120)
 })
-scene.onOverlapTile(SpriteKind.Player, assets.tile`transparency16`, function (sprite, location) {
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestClosed, function (sprite, location) {
     game.over(true)
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleRedCrystal, function (sprite, location) {
+    game.showLongText("出口はどこだ", DialogLayout.Bottom)
+    game.showLongText("そろそろ出口だといいけど。。。", DialogLayout.Bottom)
     tiles.setTilemap(tilemap`レベル3`)
+    tiles.placeOnRandomTile(mySprite, sprites.swamp.swampTile9)
     info.stopCountdown()
     info.startCountdown(45)
-    tiles.placeOnRandomTile(mySprite, sprites.swamp.swampTile9)
-    tiles.placeOnRandomTile(mySprite2, sprites.dungeon.collectibleInsignia)
 })
 let mySprite2: Sprite = null
 let mySprite: Sprite = null
@@ -42,7 +107,6 @@ mySprite = sprites.create(img`
     . . . f f f f f f . . . 
     . . . f f . . f f . . . 
     `, SpriteKind.Player)
-mySprite.say("えなに、は。", 950)
 mySprite2 = sprites.create(img`
     . . . . . . . . . b 5 b . . . . 
     . . . . . . . . . b 5 b . . . . 
@@ -61,16 +125,54 @@ mySprite2 = sprites.create(img`
     . . c b d d d d d 5 5 5 b b . . 
     . . . c c c c c c c c b b . . . 
     `, SpriteKind.Food)
-controller.moveSprite(mySprite, 100, 100)
 tiles.setTilemap(tilemap`level_1`)
 tiles.placeOnRandomTile(mySprite, sprites.dungeon.floorDark2)
 tiles.placeOnRandomTile(mySprite2, sprites.dungeon.collectibleInsignia)
 scene.cameraFollowSprite(mySprite)
+game.setDialogCursor(img`
+    . . . . f f f f . . . . 
+    . . f f e e e e f f . . 
+    . f f e e e e e e f f . 
+    f f f f 4 e e e f f f f 
+    f f f 4 4 4 e e f f f f 
+    f f f 4 4 4 4 e e f f f 
+    f 4 e 4 4 4 4 4 4 e 4 f 
+    f 4 4 f f 4 4 f f 4 4 f 
+    f e 4 d d d d d d 4 e f 
+    . f e d d b b d d e f . 
+    . f f e 4 4 4 4 e f f . 
+    e 4 f b 1 1 1 1 b f 4 e 
+    4 d f 1 1 1 1 1 1 f d 4 
+    4 4 f 6 6 6 6 6 6 f 4 4 
+    . . . f f f f f f . . . 
+    . . . f f . . f f . . . 
+    `)
+game.showLongText("え、あひる・・", DialogLayout.Bottom)
+game.showLongText("どうゆうこと", DialogLayout.Bottom)
 info.startCountdown(5)
+controller.moveSprite(mySprite, 100, 100)
 forever(function () {
     mySprite2.follow(mySprite)
     if (mySprite.overlapsWith(mySprite2)) {
-        game.splash("ギャーーーー")
+        game.showLongText("ギャーーー", DialogLayout.Bottom)
+        game.setDialogCursor(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `)
         game.splash("アヒルに食われた")
         game.over(false)
     }
